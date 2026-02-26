ea4565239cd4124c34f1ffe07218fb8ccb6c98f1
fortifyos <fortifylabs@icloud.com>
2026-02-26 14:17:10 -0500

    chore: checkpoint current local changes

--- dist/assets/index-DO3Q3hEp.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-B-FT0xrO.js","assets/index-a34zLPwc.js"])))=>i.map(i=>d[i]);
-import{_ as r}from"./index-C6ND9He7.js";import{r as o}from"./index-a34zLPwc.js";const g=o("SecureStoragePlugin",{web:()=>r(()=>import("./web-B-FT0xrO.js"),__vite__mapDeps([0,1])).then(e=>new e.SecureStoragePluginWeb)});export{g as SecureStoragePlugin};
